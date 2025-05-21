345568fba3e489a84c6619acc48cf49aee93ef72
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2025-05-21 12:39:46 -0300

    ResourceErrorMessage

--- src/BarberBoss.Api/Filters/ExceptionFilter.cs
+++ src/BarberBoss.Api/Filters/ExceptionFilter.cs
@@ -1,4 +1,5 @@
 ﻿using BarberBoss.Communication.Responses;
+using BarberBoss.Exception;
 using BarberBoss.Exception.ExceptionsBase;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -41,7 +42,7 @@ namespace BarberBoss.Api.Filters
 
         private void ThrowUnkowError(ExceptionContext context)
         {
-            var errorResponse = new ResponseErrorJson("Unknown error");
+            var errorResponse = new ResponseErrorJson(ResourceErrorMessages.UNKNOWN_ERROR);
 
             context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
             context.Result = new ObjectResult(errorResponse);
