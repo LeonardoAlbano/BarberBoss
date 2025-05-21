c07bf57a500c61e0b0190078310e4d0017b221d2
Leonardo Albano <leonardoalbanom@yahoo.com.br>
2025-05-21 15:25:31 -0300

    config unitTests

---
+++ tests/Validators.Tests/Expenses/Register/RegisterExpenseValidatorTests.cs
@@ -0,0 +1,23 @@
+﻿using BarberBoss.Application.UseCases.Expenses.Register;
+using BarberBoss.Communication.Requests;
+using CommonTestUtilities.Requests;
+
+namespace Validators.Tests.Expenses.Register
+{
+    public class RegisterExpenseValidatorTests
+    {
+    [Fact]
+        public void Success()
+        {
+            //Arrange
+            var validator = new RegisterExpenseValidator();
+            var request = RequestRegisterExpenseJsonBuilder.Build();
+
+            //Act
+            var result = validator.Validate(request);
+
+            //Assert
+            Assert.True(result.IsValid);
+        }
+    }
+}
